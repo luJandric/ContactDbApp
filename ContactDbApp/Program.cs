763b4998c0170883bafb5d05c1e2dc3a778bbb81
Luka Jandric <luka.ja1994@gmail.com>
2021-08-03 10:35:27 +0200

    Add project files.

---
+++ ContactDbApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ContactDbApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ContactDbLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ContactDbLib
+{
+    public class Class1
+    {
+    }
+}
