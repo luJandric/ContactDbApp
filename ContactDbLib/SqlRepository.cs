b8ffc5946b6c466d7072eadf0e41f15ce0f05ae6
Tamas Vicze <tomvicze96@hotmail.com>
2021-08-03 13:38:51 +0200

    Changed the methods value from Internal to Public. Added Resources folder to have the .sql files inside i for the . sql files.

--- ContactDbLib/SqlRepository.cs
+++ ContactDbLib/SqlRepository.cs
@@ -4,9 +4,9 @@ using Microsoft.Data.SqlClient;
 
 namespace ContactDbLib
 {
-    public static class SqlRepository 
+    public static class SqlRepository
     {
-        static void CreateContact(string ssn, string firstName, string lastName)
+        public static void CreateContact(string ssn, string firstName, string lastName)
         {
             string connectionString =
                 @"Server = (localdb)\MSSQLLocalDB; " +
@@ -19,6 +19,37 @@ namespace ContactDbLib
             command.CommandText =
                 "INSERT INTO Contact; " +
                 "VALUES {}(";
+        }
+
+
+         public static int ReadContact(int id)
+        {
+            string connectionString =
+                @"Server = (localdb)\MSSQLLocalDB; " +
+                "Database = ContactDb; " +
+                "Integrated Security = true";
+
+            using SqlConnection connection = new(connectionString);
+            SqlCommand command = connection.CreateCommand();
+
+            command.CommandText =
+                "SELECT *; " +
+                "WHERE Contact.ID = @ID ";
+            command.Parameters.AddWithValue("@ID", id);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                Console.WriteLine($"You're looking for : {id}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return id;
         }
     }
 }
