a0a24d4b5171179895ca699b17f741ad4ca1a395
Luka Jandric <luka.ja1994@gmail.com>
2021-08-03 11:39:36 +0200

    Created SqlRepository.cs

---
+++ ContactDbLib/SqlRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+
+namespace ContactDbLib
+{
+    public static class SqlRepository 
+    {
+        static void CreateContact(string ssn, string firstName, string lastName)
+        {
+            // Currently Empty
+        }
+    }
+}
