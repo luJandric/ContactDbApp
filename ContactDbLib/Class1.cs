999e0e2b184ec82d22312ae53f749a5364e3c321
Tamas Vicze <tomvicze96@hotmail.com>
2021-08-03 11:20:39 +0200

    Created Content class. Added properties to Content class.

--- ContactDbLib/Class1.cs
+++ ContactDbLib/Class1.cs
@@ -2,7 +2,38 @@
 
 namespace ContactDbLib
 {
-    public class Class1
+    public class Contact
     {
+        private int _ID;
+        private string _SSN;
+        private string _FirstName;
+        private string _LastName;
+
+        public int ID
+        {
+            get => ID;
+            set => _ID = ID;
+        }
+
+        public string SSN
+        {
+            get => SSN;
+
+            set => _SSN = SSN;
+        }
+
+        public string FirstName
+        {
+            get => FirstName;
+
+            set => _FirstName = FirstName;
+        }
+
+        public string LastName
+        {
+            get => LastName;
+
+            set => _LastName = LastName;
+        }
     }
 }
